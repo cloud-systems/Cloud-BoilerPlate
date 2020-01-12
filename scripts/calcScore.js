60dfe65a30c0f32a685cda98bf40d2d14ac21a8c
Amruth Nare <amruthnare.1@gmail.com>
2020-01-12 15:30:27 -0500

    g

--- scripts/calcScore.js
+++ scripts/calcScore.js
@@ -5,6 +5,6 @@ function calculate(sleep, calories, exercise){
     var totalScore;
     
     totalScore = (4.1625*sleep) + (.01665*calories) + (1.11*exercise)
-    return totalScore
+    return totalScore;
 
 }
