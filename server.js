4f8f978a8116aabd9f73f15b4708ca4ffe1f2e10
Amruth Nare <amruthnare.1@gmail.com>
2020-01-12 11:12:50 -0500

    Co-authored-by: ronoy sarkar <ronoysarkar.mail@gmail.com> Co-authored-by: Epicness35 <srujanpenikela@gmail.com>

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = '3000'
-app.use(express.static(__dirname + "/scripts"))
+const fs = require('fs');
+const readline = require('readline');
+const { google } = require('googleapis');
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+const TOKEN_PATH = 'token.json';
+
+const port = '3500'
+app.use(express.static(__dirname + "/scripts")) 
+app.use(express.static("/pages")) 
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -10,10 +17,92 @@ app.get('/', (req, res) => {
 })
 
 app.get('/loading', (req, res) => {
-    console.log("is this working");
     res.sendFile(path.join(__dirname, '/pages/loading.html'));
 })
 
-app.listen(port, () => {
-    console.log('Server is running on http://localhost:3000');
+app.get('/calendar', (req, res) => {
+    res.sendFile(path.join(__dirname, '/pages/calendar.html'));
 })
+
+app.get('/data', (req, res) => {
+    res.sendFile(path.join(__dirname, '/pages/data.html'));
+})
+
+app.get('/nav', (req, res) => {
+    res.sendFile(path.join(__dirname, '/pages/nav.html'));
+})
+
+app.get('/log', (req, res) => {
+    res.sendFile(path.join(__dirname, '/pages/log.html'));
+})
+
+app.listen(port, () => {
+    console.log('Server is running on http://localhost:3500');
+})
+
+fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Calendar API.
+    authorize(JSON.parse(content), listEvents);
+});
+
+function authorize(credentials, callback) {
+    const { client_secret, client_id, redirect_uris } = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, (err, token) => {
+        if (err) return getAccessToken(oAuth2Client, callback);
+        oAuth2Client.setCredentials(JSON.parse(token));
+        callback(oAuth2Client);
+    });
+}
+
+function getAccessToken(oAuth2Client, callback) {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+    });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    rl.question('Enter the code from that page here: ', (code) => {
+        rl.close();
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) return console.error('Error retrieving access token', err);
+            oAuth2Client.setCredentials(token);
+            // Store the token to disk for later program executions
+            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                if (err) return console.error(err);
+                console.log('Token stored to', TOKEN_PATH);
+            });
+            callback(oAuth2Client);
+        });
+    });
+}
+
+function listEvents(auth) {
+  const calendar = google.calendar({version: 'v3', auth});
+  calendar.events.list({
+    calendarId: 'primary',
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const events = res.data.items;
+    if (events.length) {
+      console.log('Upcoming 10 events:');
+      events.map((event, i) => {
+        const start = event.start.dateTime || event.start.date;
+        console.log(`${start} - ${event.summary}`);
+      });
+    } else {
+      console.log('No upcoming events found.');
+    }
+  });
+}
